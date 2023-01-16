98fb39757d29610dd99caabb14a5386f559243f8
Snow Cactus <camdenz2k3@gmail.com>
2023-01-16 16:48:31 -0600

    added foreignkeys to index model

--- models/index.js
+++ models/index.js
@@ -4,24 +4,24 @@ import product from "./product";
 import service from "./service";
 
 user.hasMany(order, {
-
+    foreignKey: "user_id"
 });
 order.belongsTo(user, {
-
+    foreignKey: "user_id"
 });
 
 order.hasMany(product, {
-    
+    foreignKey: "order_id"    
 });
 product.belongsTo(order, {
-
-})
+    foreignKey: "order_id"
+});
 
 order.hasMany(service, {
-    
+   foreignKey: "order_id" 
 });
 service.belongsTo(order, {
-
-})
+    foreignKey: "order_id"
+});
 
 export { user, order, product, service };
