02b45053080d8b9bb1f502a5400df7e73945c84e
Snow Cactus <camdenz2k3@gmail.com>
2023-02-01 17:49:21 -0600

    added user_id to order.js

--- models/order.js
+++ models/order.js
@@ -27,6 +27,15 @@ order.init(
 				key: "id",
 			},
 		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			references: {
+				model: 'user',
+				key: 'id',
+		  	},
+		}
     },
     {
 		sequelize,
